f758c295effb507279dcff0e0420efcc3751daee
gubaojian <gubaojian@163.com>
2025-06-09 16:16:27 +0800

    更新publish脚本

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 export GPG_TTY=$(tty)
-mvn clean package -P gpg -DskipTests   
+mvn clean package -P gpg -DskipTests
+## https://central.sonatype.com/publishing/deployments
 mvn deploy -DskipTests -P gpg -Dgpg.batch=true
